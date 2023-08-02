5304d971eea7552578601944ef3ba0aace532108
lahariyandapalli <ylahari6@gmail.com>
2023-08-02 18:00:26 +0530

    updated

--- src/Components/secondPage/Menu3/SearchMenu2.js
+++ src/Components/secondPage/Menu3/SearchMenu2.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import './SearchMenu2.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function SearchMenu2() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+
   // Add your implementation for searching and displaying results here
 
   const handleSubmit = (e) => {
@@ -14,17 +17,27 @@ function SearchMenu2() {
   return (
     <div className="search-menu2-container">
       <h3>Menu 3 - Search</h3>
-      <form onSubmit={handleSubmit} className="search-menu2-inputs">
-        <label>
-          First Name:
-          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </label>
-        <button type="submit" className="search-menu2-button">Submit</button>
-      </form>
+      <Form onSubmit={handleSubmit} className="search-menu2-inputs">
+        <Form.Group controlId="firstName">
+          <Form.Label>First Name:</Form.Label>
+          <Form.Control
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="Enter first name"
+          />
+        </Form.Group>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last Name:</Form.Label>
+          <Form.Control
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Enter last name"
+          />
+        </Form.Group>
+        <Button type="submit" className="search-menu2-button">Submit</Button>
+      </Form>
       {/* Add code to display search results here */}
     </div>
   );
